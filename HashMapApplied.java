a52bc9e0a43fc1eda835c3308332fa1a77847c27
Gabriel Fernandes Silva <gabriel_fsbs2@hotmail.com>
2024-11-10 16:42:21 -0300

    1. Two Sum

--- HashMapApplied.java
+++ HashMapApplied.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapApplied {
     /*
@@ -28,15 +30,18 @@ public class HashMapApplied {
 
 
     public static void main(String[] args) {
-        String ex1 = "leetcode";
-
+        //387. First Unique Character in a String
+       /* String ex1 = "leetcode";
         String ex2 = "loveleetcode";
-
         String ex3 = "aabb";
-
         System.out.println(firstUniqChar(ex1));
         System.out.println(firstUniqChar(ex2));
-        System.out.println(firstUniqChar(ex3));
+        System.out.println(firstUniqChar(ex3));*/
+
+        //1.Two Sum
+        int[] numbers = {2,5,5,11};
+        System.out.println(Arrays.toString(htTwoSum(numbers, 10)));
+
     }
 
     //my solution
@@ -47,10 +52,10 @@ public class HashMapApplied {
         }
 
         for (int i = 0; i < s.length(); i++) {
-            if(map.get(s.charAt(i)) == 1)
+            if (map.get(s.charAt(i)) == 1)
                 return i;
         }
-        
+
         return -1;
     }
 
@@ -68,5 +73,64 @@ public class HashMapApplied {
         }
 
         return -1;
+    }
+
+    /*
+      1. Two Sum
+      Given an array of integers nums and an integer target,
+      return indices of the two numbers such that they add up to target.
+      You may assume that each input would have exactly one solution, and you may not use the same element twice.
+      You can return the answer in any order.
+
+      Example 1:
+      Input: nums = [2,7,11,15], target = 9
+      Output: [0,1]
+      Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+      Example 2:
+      Input: nums = [3,2,4], target = 6
+      Output: [1,2]
+
+      Example 3:
+      Input: nums = [3,3], target = 6
+      Output: [0,1]
+
+     */
+
+    //my own solution
+    //Brute Force
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[0] = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                result[1] = nums[j];
+                if(result[0] + result[1] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    //Hash Table
+    public static int[] htTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
     }
 }
